3207b0b95dc0c82683ad7b409f8415eb799e73d1
Hamsa <hamsam0097@gmail.com>
2025-06-14 12:26:25 +0300

    add delete project logic, has error with no handlind

--- src/index.js
+++ src/index.js
@@ -75,6 +75,16 @@ main.addEventListener('click', (event) => {
       renderProjectPage(projectObject);
   }
 
+  //Delete On Edit Project Page
+  if (event.target.classList.contains('deleteProjectButton')){
+    if (Project.Projects.length ===1) throw new Error('Cannot Delete Final Project');
+    const projectTitle = document.getElementsByClassName('projectTitle')[0].innerText;
+    const projectObject = Project.filterProjectArrayByTitle(projectTitle);
+    Project.deleteObject(Project.Projects, projectTitle)
+    updateProjectSideBar(Project.Projects);
+    renderProjectPage(Project.Projects[0]);
+  }
+
   //Open To Do Detailed View
   const toDoContainer = event.target.closest('.toDoSection');
   if (toDoContainer){
