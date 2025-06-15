70fc427d133c8eb3d88c820c0de3a2cb3a2654d9
Hamsa <hamsam0097@gmail.com>
2025-06-15 18:20:04 +0300

    Add built dist folder

--- webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-
-module.exports = merge(common, {
-  mode: "production",
-  devtool: false, // no source maps or you can use 'source-map' if you want
-  // add production-specific optimizations here if needed
-});
